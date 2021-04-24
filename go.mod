ac07d321b54e1862806f726f6892d701063da36f
andreicalinciuc <scoala.andrei@yahoo.com>
2021-04-24 19:15:18 +0300

    create project

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/Take-A-Seat/restaurants
+
+go 1.14
+
+require (
+	github.com/ProAdminServ/auth v0.0.0-20201110085145-52a36562eef7
+	github.com/Take-A-Seat/auth v0.0.0-20210424154909-0ef529724d93
+	github.com/aws/aws-sdk-go v1.38.25 // indirect
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/gin v1.7.1
+	github.com/kr/text v0.2.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/Take-A-Seat/auth/validatorAuth"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"log"
+	"os"
+	"time"
+)
+
+var mongoHost = "takeaseat.knilq.mongodb.net"
+var mongoUser = "admin"
+var mongoPass = "p4r0l4"
+var mongoDatabase = "TakeASeat"
+
+func main() {
+	port := os.Getenv("TAKEASEAT_RESTAURANTS_PORT")
+	if port == "" {
+		port = "9215"
+	}
+
+	//gin.SetMode(gin.ReleaseMode)
+	gin.SetMode(gin.DebugMode)
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "DELETE", "GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accepts", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		MaxAge:           1 * time.Minute,
+		AllowCredentials: true,
+	}))
+
+	//privateRoutesUsers need Authorization token in header
+	protectedUsers := router.Group("/restaurants")
+	protectedUsers.Use(validatorAuth.AuthMiddleware("http://www.api.takeaseat.site/auth/isAuthenticated"))
+	{
+
+	}
+
+	if err := router.Run(":" + port); err != nil {
+		log.Fatal("Port already in use!")
+	}
+
+}
